a958ccca50143a8e77d677b9c85a04e8b77d929a
Valraevn <131688256+Valraevn@users.noreply.github.com>
2023-12-06 05:53:50 +0000

    variables.tf
    This one determines what to prompt for when running a 'terraform apply'

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vsphere_user" {
+  description = "Login"
+}
+
+variable "vsphere_password" {
+  description = "Password"
+}
+
+variable "vsphere_server" {
+  description = "vSphere IP addr"
+}
